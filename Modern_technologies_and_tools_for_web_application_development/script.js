fe205d7af6637413d8396469537c1992ebda6899
RumIsDeath <grovoy.545@bk.ru>
2024-01-26 08:41:11 +0900

    commit

---
+++ Modern_technologies_and_tools_for_web_application_development/script.js
@@ -0,0 +1,22 @@
+// JavaScript код с созданием переменных 
+/* 
+Автор:
+Ершов Матвей 
+*/
+let myName = 'Матвей'
+let mySurename = 'Ершов'
+let myFavoriteDrink ='чай'
+let myFavoriteAnimal = 'краб'
+let myFavoriteProgrammingLanguage = 'JavaScript'
+
+console.log(myName)
+console.log(mySurename)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
+
+
+
+
+
+
