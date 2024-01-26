be6b0376875425eab66557af7055ca25c506b113
RumIsDeath <grovoy.545@bk.ru>
2024-01-26 09:40:50 +0900

    commit

---
+++ Acsesize2/data-types.js
@@ -0,0 +1,22 @@
+const number = 9007199254740991
+const bigint = 234576897895762723456435743868732643279n
+const dotnumer = 23.213
+const boole = true
+const text = 'text'
+const nin = NaN
+const nol = null
+const und = undefined
+const obj = new Object()
+const sim = Symbol()
+
+console.log('number', number, typeof number)
+console.log('bigint', bigint, typeof bigint)
+console.log('dotnumber', dotnumer, typeof dotnumer)
+console.log('boole', boole, typeof boole)
+console.log('text', text, typeof text)
+console.log('nin', nin, typeof nin)
+console.log('nol', nol, typeof nol)
+console.log('und', und, typeof und)
+console.log('obj', obj, typeof obj)
+console.log('sim', sim, typeof sim)
+
---
+++ Acsesize2/index.js
@@ -0,0 +1,11 @@
+let numero = 45
+let texto = '12'
+let boolka = true
+
+numero = '45'
+texto = 12
+boolka = null
+
+alert(numero)
+alert(texto)
+alert(boolka)
