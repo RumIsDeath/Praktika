c0fee2472243c69698c475d7455211dfc33f1725
RumIsDeath <grovoy.545@bk.ru>
2024-01-26 10:16:25 +0900

    commit

--- Acsesize2/index.js
+++ Acsesize2/index.js
@@ -1,4 +1,4 @@
-let numero = 45
+/* let numero = 45
 let texto = '12'
 let boolka = true
 
@@ -8,4 +8,44 @@ boolka = null
 
 alert(numero)
 alert(texto)
-alert(boolka)
+alert(boolka) */
+
+let ageOfPerson1 = 18
+let ageOfPerson2 = '20'
+
+ageOfPerson2 = ageOfPerson1
+console.log(ageOfPerson2, typeof ageOfPerson2)
+
+let nameOfAnimal1 = null
+let nameOfAnimal2 = 'Charlie'
+
+nameOfAnimal2 = nameOfAnimal1
+console.log(nameOfAnimal2, typeof nameOfAnimal2)
+
+const bestProgramingLanguage1 = 'JavaScript'
+const bestProgramingLanguage2 = 'Java'
+
+console.log('Переменную с const переопределить нельзя')
+console.log(bestProgramingLanguage1, typeof bestProgramingLanguage1)
+console.log(bestProgramingLanguage2, typeof bestProgramingLanguage2)
+
+let initialValue1 
+let initialValue2 = 0
+
+initialValue2 = initialValue1
+console.log(initialValue2, typeof initialValue2)
+
+let isJavaScriptProgrammer1 = true
+let isJavaScriptProgrammer2 = false
+
+console.log('var является утсаревшим')
+isJavaScriptProgrammer2 = isJavaScriptProgrammer1
+console.log(isJavaScriptProgrammer2, typeof isJavaScriptProgrammer2)
+
+let helloText1 = 'Hello!'
+let helloText2 = 'Привет!'
+
+helloText2 = helloText1
+console.log(helloText2, typeof helloText2)
+
+
