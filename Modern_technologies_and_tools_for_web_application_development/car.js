b4b70924d766c598c437b348f7b57b805ea8bdea
RumIsDeath <grovoy.545@bk.ru>
2024-01-26 09:01:58 +0900

    commit

---
+++ Modern_technologies_and_tools_for_web_application_development/car.js
@@ -0,0 +1,23 @@
+const carName = 'Mitsubishi'
+let maxCarSpeed = '250 км/ч'
+let carOwner = 'Владимир Анатольевичь Шпак'
+
+alert(carName)
+alert(maxCarSpeed)
+alert(carOwner)
+
+maxCarSpeed = '235 км/ч'
+carOwner = 'Егор Денисович Бурченко'
+
+console.log('У машины изменилась скорость', maxCarSpeed)
+console.log('У машины изменился владелец', carOwner)
+
+maxCarSpeed = '215 км/ч'
+carOwner = 'Максим Кирилович Шмидт'
+
+console.log('У машины изменилась скорость', maxCarSpeed)
+console.log('У машины изменился владелец', carOwner)
+
+
+
+
