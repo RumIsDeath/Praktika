ac74a9fb2e6b41144d4ed4bcee12f8564969d6de
RumIsDeath <grovoy.545@bk.ru>
2024-02-01 05:38:46 +0900

    commit

---
+++ Acsesize4/number1.js
@@ -0,0 +1,11 @@
+const myName = 'Матвей'
+const programingLanguage = 'JavaScript'
+const courseCrearoeName = 'Никита Михайлович'
+const reasonText = 'я хочу зарабатывать много деняг'
+const numberOfMonth = 1
+console.log(`Всем привет меня зовут, ${myName}. Сейчас я изучаю язык програмирования ${programingLanguage} у ${courseCrearoeName}.`)
+console.log(`Я хочу стать веб-разработчиком, потому-что ${reasonText}. До этого я изучал ${programingLanguage} ${numberOfMonth} месяц. Я уверен, что пройду данный курс до конца!`)
+
+
+
+
